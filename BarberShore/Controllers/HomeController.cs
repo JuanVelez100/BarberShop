06d31dc82c7ab2953d69aba2e1490787c2f0e05b
Juan David Vasquez Velez <91702282+JuanVelez100@users.noreply.github.com>
2021-10-01 13:52:33 -0500

    Add files via upload

---
+++ BarberShore/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BarberShore.Models;
+using BarberShoreLogic.Logic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BarberShore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            PersonaLogic personaLogic = new PersonaLogic();
+
+            return View(personaLogic.GetAllPerson());
+        }
+
+
+
+
+
+    }
+}
---
+++ BarberShore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BarberShore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
