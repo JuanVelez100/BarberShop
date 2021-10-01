242bed636fc0467da00d5750413fd97a3f343fee
Juan David Vasquez Velez <91702282+JuanVelez100@users.noreply.github.com>
2021-10-01 13:48:16 -0500

    Entity
    Entity

---
+++ BarberEntity/Entity/PersonaEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberEntity.Entity
+{
+    public class PersonaEntity
+    {
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Celular { get; set; }
+        public string Correo { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
